f28e9c91c0a7a48dc2f6b6c4e456c9b848d01ce2
EduLebre <eduardolebre2008@gmail.com>
2026-09-15 00:48:40 -0300

    feat: adiciona base da conexão MySQL

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import mysql.connector
 from flask import Flask
 
 app = Flask(__name__)
 
 def connect_db():
-    pass
+    return mysql.connector.connect()
 
 @app.route("/imoveis", methods=["GET"])
 def listar_imoveis():
